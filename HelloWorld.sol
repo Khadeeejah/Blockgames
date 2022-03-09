a36eb67ad14f65457caf1076da9aef001b280446
Khadeeejah <wuraola81@gmail.com>
2022-03-09 11:45:11 +0100

    first-commit

---
+++ HelloWorld.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+// pragma solidity 0.8.0;
+// pragma solidity ^0.8.0;
+
+// pragma solidity >=0.4.22 <0.9.0;
+
+pragma solidity ^0.8.0;
+
+contract HelloWorld{
+    string public greeting = "Hello World";
+    // greeting()
+    // uint age = 20;
+    // int age1 = 22;
+}
